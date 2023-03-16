a86d8d3ce0848b871e543f4b5862b0b55f36fad0
Christiannjhay <cejysugarol@addu.edu.ph>
2023-03-16 15:31:17 +0800

    update

--- lib/detailScreen.dart
+++ lib/detailScreen.dart
@@ -4,22 +4,68 @@ import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
 
-  final Map<String, dynamic> data;
+  final Map < String, dynamic > data;
 
-  const DetailScreen({required this.data});
+  const DetailScreen({
+    required this.data
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 9, 26, 47),
         title: Text(data['name']),
       ),
-      body: Center(
-        child: Text(data['description'],
-        style: TextStyle(
-          color: Colors.white
-        ),),
-      ),
+      body: Container(
+        child: Stack(
+          children: < Widget > [
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.all(20),
+                  child: Container(
+                    child: Image(image: NetworkImage(data['logo']), )
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(20),
+                  child: Text(data['description'], style: TextStyle(
+                    color: Colors.white
+                  ),)
+                ),
+                SizedBox(height: 20),
+                Container(
+                      width: 340,
+                      height: 60,
+                      child: TextButton(
+                        onPressed: () {
+                          print('Joined');
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll < Color > (Color.fromARGB(255, 251, 183, 24)),
+                          shape: MaterialStateProperty.all < RoundedRectangleBorder > (
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+
+                            )
+                          )
+                        ),
+                        child: Text(
+                          'Join Club',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: "Noopla"
+                          ),
+
+                        ),
+                      ),
+                    ), 
+                
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
