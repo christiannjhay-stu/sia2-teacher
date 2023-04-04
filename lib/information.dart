a8e962316aaf4231cac8f060722466a7a52b72af
Christiannjhay <cejysugarol@addu.edu.ph>
2023-04-05 01:58:54 +0800

    updated UI

--- lib/information.dart
+++ lib/information.dart
@@ -21,7 +21,9 @@ class _InformationState extends State<Information> {
     String? email = FirebaseAuth.instance.currentUser?.email;
     context.read<UserProvider>().setEmail(email!);
        return Scaffold(
-           appBar: AppBar(title: const Text('My Information'),),
+           appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 9, 26, 47),
+            title: const Text('My Information'),),
            body: StreamBuilder<QuerySnapshot>(
             
         stream: FirebaseFirestore.instance
@@ -51,14 +53,66 @@ class _InformationState extends State<Information> {
               Map<String, dynamic> data = document.data() as Map<String, dynamic>;
              
               return ListTile(
-                title: Text(data['name'],
+                title: Center(
+                  child: Text(data['name'],
                 style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
                   color: Colors.white
-                ),),
-                subtitle: Text(data['section'],style: TextStyle(
-                  color: Colors.white
-                ),),
-                trailing: IconButton(
+                ),), 
+                ),
+                subtitle: Column(
+                  children: <Widget>[
+                    SizedBox(height: 10,),
+                    Text('Email', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['email'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                    SizedBox(height: 10,),
+                    Text('Grade Level', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['grade'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                    SizedBox(height: 10,),
+                    Text('Section', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['section'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                    SizedBox(height: 10,),
+                    Text('Address', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['address'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                     SizedBox(height: 10,),
+                    Text('Contact Number', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['contact'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                    SizedBox(height: 10,),
+                    Text('Gender', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['gender'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                    SizedBox(height: 10,),
+                    Text('Birthday', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['birthday'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                    SizedBox(height: 10,),
+                    Text('Mother Tongue', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['MT'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+                      SizedBox(height: 10,),
+                    Text('Religion', style: TextStyle(color: Color.fromARGB(255, 251, 183, 24), fontWeight: FontWeight.bold ),),
+                    Text(data['religion'], style: TextStyle(
+                      color: Colors.white
+                    ), ),
+
+                    
+                  ],
+                ),
+                 trailing: IconButton(
                   icon: Icon(Icons.edit),
                    onPressed: () {
                           Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
@@ -68,8 +122,7 @@ class _InformationState extends State<Information> {
                           
                           
                         },
-                ),
-                
+                )
               );
             },
           );
@@ -92,10 +145,34 @@ class EditTeacherScreen extends StatefulWidget {
 
 class _EditTeacherScreenState extends State<EditTeacherScreen> {
   final _formKey = GlobalKey<FormState>();
+
   final _nameController = TextEditingController();
   final _subjectController = TextEditingController();
+  final _emailController = TextEditingController();
+   final _addressController = TextEditingController();
+  final _contactController = TextEditingController();
+   final _genderController = TextEditingController();
+     final _birthdayController = TextEditingController();
+        final _GradeController = TextEditingController();
+        final _MTController = TextEditingController();
+        final _ReligionController = TextEditingController();
+
+
+
+
+
   late String _initialName;
   late String _initialSubject;
+  late String _initialEmail;
+  late String _initialAddress;
+
+
+ late String _initialContact;
+  late String _initialGender;
+  late String _initialBirthday;
+  late String _initialGrade;
+  late String _initialMT;
+  late String _initialReligion;
 
   @override
   void initState() {
@@ -110,9 +187,28 @@ class _EditTeacherScreenState extends State<EditTeacherScreen> {
         Map<String, dynamic> data =
             documentSnapshot.data() as Map<String, dynamic>;
         _nameController.text = data['name'];
+        _emailController.text = data['email'];
+        _GradeController.text = data['grade'];
         _subjectController.text = data['section'];
+        _addressController.text = data['address'];
+        _contactController.text = data['contact'];
+        _genderController.text = data['gender'];
+        _birthdayController.text = data['birthday'];
+        _MTController.text = data['MT'];
+        _ReligionController.text = data['religion'];
+
+
+
+
         _initialName = data['name'];
-        _initialSubject = data['section'];
+        _initialAddress = data['address'];
+        _initialContact = data['contact'];
+        _initialGender = data['gender'];
+        _initialBirthday = data['birthday'];
+        _initialMT = data['MT'];
+        _initialReligion = data ['religion'];
+
+        
       }
     });
   }
@@ -121,15 +217,29 @@ class _EditTeacherScreenState extends State<EditTeacherScreen> {
     if (_formKey.currentState!.validate()) {
       try {
         String newName = _nameController.text;
-        String newSubject = _subjectController.text;
+        String newAddress = _addressController.text;
+        String newContact = _contactController.text;
+        String newGender = _genderController.text;
+        String newBirthday = _birthdayController.text;
+        String newMT = _MTController.text;
+        String newReligion = _ReligionController.text;
+
+
         // Only update the fields that have changed
-        if (newName != _initialName || newSubject != _initialSubject) {
+        if (newName != _initialName || newAddress != _initialAddress || newContact != _initialContact || newGender != _initialGender
+        || newBirthday != _initialBirthday || newMT != _initialMT || newReligion != _initialReligion) {
           await FirebaseFirestore.instance
               .collection('teachers')
               .doc(widget.documentId)
               .update({
             if (newName != _initialName) 'name': newName,
-            if (newSubject != _initialSubject) 'section': newSubject,
+            if (newAddress != _initialAddress) 'address': newAddress,
+            if (newContact != _initialContact) 'contact': newContact,
+            if (newGender != _initialGender) 'gender': newGender,
+            if (newBirthday != _initialBirthday) 'birthday': newBirthday,
+            if (newMT != _initialMT) 'MT': newMT,
+            if (newReligion != _initialReligion) 'religion': newReligion,
+            
           });
         }
         Navigator.pop(context); // Navigate back to the previous screen
@@ -143,7 +253,8 @@ class _EditTeacherScreenState extends State<EditTeacherScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Teacher'),
+        backgroundColor: Color.fromARGB(255, 9, 26, 47),
+        title: Text('Edit Information'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -174,7 +285,37 @@ class _EditTeacherScreenState extends State<EditTeacherScreen> {
                   return null;
                 },
               ),
+               TextFormField(
+                enabled: false,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Email',
+                ),
+                
+              ),
+              
+               TextFormField(
+                enabled: false,
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _GradeController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Grade Level',
+                ),
+                
+              ),
               TextFormField(
+                enabled: false,
                 style: TextStyle(
                   color: Colors.white
                 ),
@@ -185,16 +326,104 @@ class _EditTeacherScreenState extends State<EditTeacherScreen> {
                   ),
                   labelText: 'Section',
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a subject';
-                  }
-                  return null;
-                },
+                
+              ),
+               TextFormField(
+               
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _addressController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Address',
+                ),
+               
+              ),
+                TextFormField(
+               
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _contactController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Contact Number',
+                ),
+               
+              ),
+                TextFormField(
+               
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _genderController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Gender',
+                ),
+                
+              ),
+                TextFormField(
+               
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _birthdayController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Birthday',
+                ),
+                
+              ),
+                TextFormField(
+               
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _MTController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'MT',
+                ),
+               
+              ),
+                TextFormField(
+               
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: _ReligionController,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 251, 183, 24)
+                  ),
+                  labelText: 'Religion',
+                ),
+               
               ),
               SizedBox(height: 16.0),
               ElevatedButton(
-                onPressed: _updateTeacher,
+                onPressed: () {
+                  _updateTeacher();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    backgroundColor: Color.fromARGB(255, 23, 133, 60), // set the background color
+                    content: Text('Successfully Updated'), // set the message text
+                    duration: Duration(seconds: 2), // set the duration for how long the message will be displayed
+                  ),
+                  );
+                },
                 child: Text('Update'),
               ),
             ],
