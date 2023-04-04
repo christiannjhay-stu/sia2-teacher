f714f4b0b2d248cc138be2466fbf441b6f80e23b
Christiannjhay <cejysugarol@addu.edu.ph>
2023-04-05 07:01:55 +0800

    updated UI

--- lib/detailScreen.dart
+++ lib/detailScreen.dart
@@ -30,7 +30,56 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 9, 26, 47),
         title: Text(data['name']),
       ),
-      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      body:Center(
+        child: Stack(
+          children: <Widget>[
+            Row(
+              children: [
+                Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20, left: 115),
+              child: Text(
+                '1ST',
+                style: TextStyle(color: Colors.white),
+                    
+                  ),
+                ),
+                 Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20, left: 45),
+              child: Text(
+                '2ND',
+                style: TextStyle(color: Colors.white),
+                    
+                  ),
+                ),
+                 Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20, left: 40),
+              child: Text(
+                '3RD',
+                style: TextStyle(color: Colors.white),
+                    
+                  ),
+                ),
+                 Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20, left: 40),
+              child: Text(
+                '4TH',
+                style: TextStyle(color: Colors.white),
+                    
+                  ),
+                ),
+                 Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20, left: 50),
+              child: Text(
+                'REMARKS',
+                style: TextStyle(color: Colors.white),
+                    
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50,),
+            Padding(padding: EdgeInsets.only(top: 40, bottom: 20),
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
         stream: FirebaseFirestore.instance
             .collection('students')
             .where('name', isEqualTo: data['name'])
@@ -39,26 +88,19 @@ class DetailScreen extends StatelessWidget {
           return await query.docs.first.reference
               .collection('affiliations')
               .get();
-              
-              
-              
         }),
         builder: (BuildContext context,
             AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-             
           if (snapshot.hasError) {
             return Text('Something went wrong');
           }
 
           if (snapshot.connectionState == ConnectionState.waiting) {
-            
-            
             return Text("Loading");
           }
-       
+
           return new ListView(
-            
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+             children: snapshot.data!.docs.map((DocumentSnapshot document) {
               String documentID = document.id;
              
               Map<String, dynamic> data =
@@ -74,34 +116,77 @@ class DetailScreen extends StatelessWidget {
                         child: new Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
+                           
+
                             Row(
-                              
-                              children: <Widget>[
-                                 Text(Subject + '     ' + data['Grade1'] +'         '+ data['Grade2']+'         '+ data['Grade3']+'         '+ data['Grade4'],
-                                 style: TextStyle(
-                                  color: Color.fromARGB(246, 255, 208, 0)
-                                 ),),
-                              ],
+                             children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    width: 100,
+                                    child: Text(
+                                      Subject,
+                                      style: TextStyle(
+                                        color: Colors.white
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 70,
+                                    child: Text(
+                                      data['Grade1'],
+                                      style: TextStyle(
+                                        color: Color.fromARGB(246, 255, 208, 0)
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 70,
+                                    child: Text(
+                                      data['Grade2'],
+                                      style: TextStyle(
+                                        color: Color.fromARGB(246, 255, 208, 0)
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 70,
+                                    child: Text(
+                                      data['Grade3'],
+                                      style: TextStyle(
+                                        color: Color.fromARGB(246, 255, 208, 0)
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 70,
+                                    child: Text(
+                                      data['Grade4'],
+                                      style: TextStyle(
+                                        color: Color.fromARGB(246, 255, 208, 0)
+                                      ),
+                                    ),
+                                  ),
+                                   new IconButton(
+                                    icon: Icon(Icons.edit,
+                                    color: Colors.red,),
+                                    onPressed: () {
+                                      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                                        return EditTeacherPage(documentID: documentID, docID: docID  );
+                                      }));
+                                      
+                                    },
+                                  ),      
+                                ],
+                              )
+                             ],
                             ),
                             
                             
                           ],
                         ),
                       ),
-                      new IconButton(
-                        icon: Icon(Icons.edit,
-                        color: Colors.red,),
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-                            return EditTeacherPage(documentID: documentID, docID: docID  );
-                          }));
-                          
-                         
-
-                          
-                          
-                        },
-                      ),
+                     
                     ],
                   ),
                 ),
@@ -109,7 +194,16 @@ class DetailScreen extends StatelessWidget {
             }).toList(),
           );
         },
-      ),  
+      ),
+            
+            
+            )
+          ],
+        ),
+      )
+      
+      
+      
     );
   }
 }
