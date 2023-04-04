77b8bb37fca111dd87e88407947387dc593f506c
Christiannjhay <cejysugarol@addu.edu.ph>
2023-04-05 00:31:41 +0800

    updated logo and UI

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -52,7 +52,7 @@ Widget Footer() {
     padding: const EdgeInsets.only(top: 20),
       child: Text(
         textAlign: TextAlign.center,
-        'By using Ateneo you agree to our Term of Service and\nPrivacy Policy.',
+        'By using APJES you agree to our Term of Service and\nPrivacy Policy.',
         style: TextStyle(
           color: Colors.white,
           fontSize: 10,
@@ -241,7 +241,7 @@ class _LoginScreenState extends State < LoginScreen > {
                     Padding(
                       padding: const EdgeInsets.only(top: 80.0),
                         child: Text(
-                          'Ateneo de Davao\nUniversity\nTeacher',
+                          'Antonio Pichon Jr.\nElementary School\nTeacher',
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             color: Color.fromARGB(255, 255, 255, 255),
